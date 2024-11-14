bd64269533aa8eef9b0e82a69a18b14a5f8b35ac
Exodiamisuzu <hebuter123@foxmail.com>
2024-11-14 15:00:45 +0800

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab10.Startup))]
+namespace lab10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
